aad467ea750aa8dbaa47ead6413080116def85e5
Adam Wojnicki <wojnicki.adam1@gmail.com>
2017-11-14 00:27:02 +0100

    Added header and footer

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> >
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header>
-        <h1><?php bloginfo('name') ?></h1>
-        <span><?php bloginfo('description') ?></span>
-    </header>
+<?php get_header(); ?>
     <div class="main">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <h3><?php the_title(); ?></h3>
-        <?php endwhile; ?>
-        <?php else: ?>
-            <?php echo wpautop('Sorry, no posts here!'); ?>
-        <?php endif; ?>
+        <div class="container">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h3><?php the_title(); ?></h3>
+                <div class="meta">
+                    Created by: <?php the_author(); ?> on <?php the_date(); ?>
+                </div>
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+            <?php else: ?>
+                <?php echo wpautop('Sorry, no posts here!'); ?>
+            <?php endif; ?>
+        </div>
     </div>
-</body>
-</html>
+    
+<?php get_footer(); ?>
