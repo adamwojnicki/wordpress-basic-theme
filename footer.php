3ca2f66c2a86a13c0e428387e9b7c49ab2bb90ca
Adam Wojnicki <wojnicki.adam1@gmail.com>
2017-11-15 15:12:42 +0100

    Basic functionality for simple Wordpress theme

--- footer.php
+++ footer.php
@@ -1,8 +1,16 @@
+
+    <div class="sidebar">
+        <?php if(is_active_sidebar('sidebar')) : ?>
+            <?php dynamic_sidebar('sidebar') ?>
+        <?php endif; ?>
+    </div>
+    <div class="clr"></div>       
+</div>
 <footer>
-        <div class="container">
-            <p>&copy; <?php the_time('Y'); ?> - <?php bloginfo('name'); ?></p>
-        </div>
-    </footer>
+    <div class="container">
+        <p>&copy; <?php the_time('Y'); ?> - <?php bloginfo('name'); ?></p>
+    </div>
+</footer>
     <?php wp_footer(); ?>
 </body>
 </html>
--- single.php
+++ single.php
@@ -1,32 +1,27 @@
 <?php get_header(); ?>
-    <div class="container">    
+    <div class="container">
         <div class="main">
-
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <h3>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    <?php the_title(); ?>
                 </h3>
                 <article class="post">
                     <div class="meta">
-                        Created by 
-                        <a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>">
-                            <?php the_author(); ?> 
-                        </a>
-                        on <?php the_date(); ?>
+                        Created by: <?php the_author(); ?> on <?php the_date(); ?>
                     </div>
                     <?php if(has_post_thumbnail()) : ?>
                         <div class="thumbnail">
                             <?php the_post_thumbnail() ?>
                         </div>
                     <?php endif; ?>
-                    <?php the_excerpt(); ?>
-                    <a href="<?php the_permalink(); ?>" class="button">Read more</a>
+                    <?php the_content(); ?>
                 </article>
             <?php endwhile; ?>
             <?php else: ?>
                 <?php echo wpautop('Sorry, no posts here!'); ?>
             <?php endif; ?>
+            <?php comments_template(); ?>
         </div>
-        
+    
     
 <?php get_footer(); ?>
